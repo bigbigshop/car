8a309713a3d30f727f6d8eef68106ebffd47a5fc
bigbigshop <79006104+bigbigshop@users.noreply.github.com>
2024-02-22 13:05:41 +0800

    Add files via upload

---
+++ yg8874/testing.js
@@ -0,0 +1,9 @@
+var areaCode = '852';
+var telNumber_part1 = '9123';
+var telNumber_part2 = '1441';
+var plate1_line = "yg8874";
+var plate2_line = "";
+
+var areaCode_CN = '86';
+var telNumber_part1_CN = '1324984';
+var telNumber_part2_CN = '3070';
