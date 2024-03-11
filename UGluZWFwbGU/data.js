ccd4d877df8cffd1b6d6bcb52a1e3c824ea5db8f
bigbigshop <79006104+bigbigshop@users.noreply.github.com>
2024-03-11 23:32:18 +0800

    Create data.js

---
+++ UGluZWFwbGU/data.js
@@ -0,0 +1,9 @@
+var areaCode = '852';
+var telNumber_part1 = '9519';
+var telNumber_part2 = '7535';
+var plate1_line = "WW2626";
+var plate2_line = "";
+
+var areaCode_CN = 'xx';
+var telNumber_part1_CN = 'xxxxx';
+var telNumber_part2_CN = 'xxxx';
