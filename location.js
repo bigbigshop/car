437991f5b05881c578502c2d9d1efb202ce90775
bigbigshop <79006104+bigbigshop@users.noreply.github.com>
2024-04-22 23:48:18 +0800

    Update location.js

--- location.js
+++ location.js
@@ -14,17 +14,17 @@ window.onload = function() {
 
 	$.get(url, function(data) {
 	  console.log(data);
-	  if (data.info.prov == '香港') {
-		document.getElementById('myPhoneNumber').innerHTML = '<i class="fas fa-phone"></i> 立即致電車主';
-		document.getElementById('sendMessage').innerHTML = '<i class="fab fa-whatsapp"></i> 立即WhatsApp車主';			
-		document.getElementById('myPhoneNumber').href = "tel:+" + areaCode + telNumber_part1 + telNumber_part2;
-		document.querySelector("#sendMessage").setAttribute("onclick", "sendWhatsAppMessage()"); 
-	  } else {
+	  if (data.info.country == '中国' &&  data.info.prov != '香港' ) {
 		document.getElementById('myPhoneNumber').innerHTML = '<i class="fas fa-phone"></i> 立即致电车主(国内)';
 		document.getElementById('myPhoneNumber').href = "tel:+" + areaCode_CN + telNumber_part1_CN + telNumber_part2_CN;
 		document.getElementById('sendMessage').innerHTML = '<i class="fa-regular fa-message"></i> 发送信息';
 		var smsUrl = 'sms:' + telNumber_part1_CN + telNumber_part2_CN + '?body=' + encodeURIComponent('你好，请挪车');
 		document.querySelector("#sendMessage").setAttribute("href", smsUrl);
+	  } else {
+		document.getElementById('myPhoneNumber').innerHTML = '<i class="fas fa-phone"></i> 立即致電車主';
+		document.getElementById('sendMessage').innerHTML = '<i class="fab fa-whatsapp"></i> 立即WhatsApp車主';			
+		document.getElementById('myPhoneNumber').href = "tel:+" + areaCode + telNumber_part1 + telNumber_part2;
+		document.querySelector("#sendMessage").setAttribute("onclick", "sendWhatsAppMessage()"); 		
 	  }
 	}).fail(function() {
 	  console.log('Failed to retrieve URL');
