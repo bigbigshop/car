56c57a3109523b221d13064d7b4d607d9de9685f
bigbigshop <79006104+bigbigshop@users.noreply.github.com>
2024-10-20 23:33:41 +0800

    Add files via upload

---
+++ yg8874_eWc4ODc0/data-sample.js
@@ -0,0 +1,11 @@
+var telNumber_part1 = 'xxxx';
+var telNumber_part2 = 'xxxx';
+
+var telNumber_part1_CN = 'xxxxx';
+var telNumber_part2_CN = 'xxxx';
+
+var plate1_line = "AB1234";
+var plate2_line = "";
+
+var autoSwitch = "0";
+var region = "big6";
